c622c123738d0b43784eac065468486efaf5a911
YuhKuro <yuh.kurose@gmail.com>
2025-06-25 19:41:43 -0500

    began adding CDC functionality; will not work right now

--- firmware/tusb_config.h
+++ firmware/tusb_config.h
@@ -90,6 +90,18 @@
 #define CFG_TUD_ENDPOINT0_SIZE    64
 #endif
 
+// Enable 2 CDC classes
+#define CFG_TUD_CDC             (2)
+// Set CDC FIFO buffer sizes
+#define CFG_TUD_CDC_RX_BUFSIZE  (64)
+#define CFG_TUD_CDC_TX_BUFSIZE  (64)
+#define CFG_TUD_CDC_EP_BUFSIZE  (64)
+
+#ifndef CFG_TUD_ENDPOINT0_SIZE
+#define CFG_TUD_ENDPOINT0_SIZE  (64)
+#endif
+
+
 //------------- CLASS -------------//
 #define CFG_TUD_HID               1
 #define CFG_TUD_CDC               0
--- firmware/usb_descriptors.c
+++ firmware/usb_descriptors.c
@@ -62,6 +62,7 @@ tusb_desc_device_t const desc_device =
     .iSerialNumber      = 0x03,
 
     .bNumConfigurations = 0x01
+
 };
 
 // Invoked when received GET DEVICE DESCRIPTOR
@@ -70,6 +71,59 @@ uint8_t const * tud_descriptor_device_cb(void)
 {
   return (uint8_t const *) &desc_device;
 }
+
+
+enum {
+    ITF_NUM_CDC_0 = 0,
+    ITF_NUM_CDC_0_DATA,
+    ITF_NUM_CDC_1,
+    ITF_NUM_CDC_1_DATA,
+    ITF_NUM_TOTAL
+};
+
+// total length of configuration descriptor
+#define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + CFG_TUD_CDC * TUD_CDC_DESC_LEN)
+
+// define endpoint numbers
+#define EPNUM_CDC_0_NOTIF   0x81 // notification endpoint for CDC 0
+#define EPNUM_CDC_0_OUT     0x02 // out endpoint for CDC 0
+#define EPNUM_CDC_0_IN      0x82 // in endpoint for CDC 0
+
+#define EPNUM_CDC_1_NOTIF   0x84 // notification endpoint for CDC 1
+#define EPNUM_CDC_1_OUT     0x05 // out endpoint for CDC 1
+#define EPNUM_CDC_1_IN      0x85 // in endpoint for CDC 1
+
+
+// configure descriptor (for 2 CDC interfaces)
+uint8_t const desc_configuration[] = {
+    // config descriptor | how much power in mA, count of interfaces, ...
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x80, 100),
+
+    // CDC 0: Communication Interface - TODO: get 64 from tusb_config.h
+    TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_0, 4, EPNUM_CDC_0_NOTIF, 8, EPNUM_CDC_0_OUT, EPNUM_CDC_0_IN, 64),
+    // CDC 0: Data Interface
+    //TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_0_DATA, 4, 0x01, 0x02),
+
+    // CDC 1: Communication Interface - TODO: get 64 from tusb_config.h
+    TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_1, 4, EPNUM_CDC_1_NOTIF, 8, EPNUM_CDC_1_OUT, EPNUM_CDC_1_IN, 64),
+    // CDC 1: Data Interface
+    //TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_1_DATA, 4, 0x03, 0x04),
+};
+
+// more device descriptor this time the qualifier
+tusb_desc_device_qualifier_t const desc_device_qualifier = {
+    .bLength = sizeof(tusb_desc_device_t),
+    .bDescriptorType = TUSB_DESC_DEVICE,
+    .bcdUSB = CDC_EXAMPLE_BCD,
+
+    .bDeviceClass = TUSB_CLASS_CDC,
+    .bDeviceSubClass = MISC_SUBCLASS_COMMON,
+    .bDeviceProtocol = MISC_PROTOCOL_IAD,
+
+    .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+    .bNumConfigurations = 0x01,
+    .bReserved = 0x00
+};
 
 //--------------------------------------------------------------------+
 // HID Report Descriptor
@@ -190,9 +244,12 @@ enum {
 char const *string_desc_arr[] =
 {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  "YuhKuro",                     // 1: Manufacturer
+  "Numlocked Keyboard",              // 2: Product
   NULL,                          // 3: Serials will use unique ID if possible
+  "Pico SDK studio",            //4: CDC interface 0
+  "Custom CDC",                 //5: CDC interface 1,
+  "RPiReset"                  //6: Reset interface
 };
 
 static uint16_t _desc_str[32 + 1];
